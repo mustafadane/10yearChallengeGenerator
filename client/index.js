67a681ff47cc49793ef0041ab730aaa974b1d5e1
Mustafa Dane <mustafadane@gmail.com>
2019-01-16 11:22:31 -0600

    webpack added

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+// establishes socket connection
+import './socket'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
