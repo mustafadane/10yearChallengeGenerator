8ac39178b0ee36ff11c40211c6fe3dad5e1f8b32
Mustafa Dane <mustafadane@gmail.com>
2019-01-16 11:49:29 -0600

    background added

--- client/app.js
+++ client/app.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class App extends Component {
     render() {
         return (
-            <div>HELLLOOOOO</div>
+            <div className='container'>
+                <h1>HEllo</h1>
+            </div>
         )
     }
 }
