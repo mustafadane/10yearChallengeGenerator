6722d1c99a77e5247cad6cbdb4ab9873ac9b03f2
Mustafa Dane <mustafadane@gmail.com>
2019-01-16 11:34:50 -0600

    babel configs done

--- client/index.js
+++ client/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-// establishes socket connection
-import './socket'
-
 ReactDOM.render(
   <App />,
   document.getElementById('app')
