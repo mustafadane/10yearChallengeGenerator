72673c6251633517968a71d274649a6485ae6fbd
Mustafa Dane <mustafadane@gmail.com>
2019-01-16 16:20:59 -0600

    privacy policy added

---
+++ client/Generator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Generator extends Component {
+    render() {
+        return (
+            <div>This is Gen</div>
+        )
+    }
+}
+
+export default Generator
--- client/app.js
+++ client/app.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import Login from './Login'
+import Generator from './Generator'
 
 export class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
     render() {
         return (
             <div className='container'>
-                <h1>HEllo</h1>
+                {this.state.isLoggedIn ? <Generator /> : <Login /> }
             </div>
         )
     }
