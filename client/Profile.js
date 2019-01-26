6d03832a96aa72ec15ded6274744b7048db77918
Mustafa Dane <mustafadane@gmail.com>
2019-01-26 15:36:46 -0600

    profile page added

---
+++ client/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Profile extends Component {
+    render() {
+        return (
+            <div>this is profile</div>
+        )
+    }
+}
+
+export default Profile
