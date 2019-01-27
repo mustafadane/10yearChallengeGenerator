dd17dc2ebada7605cd0b881f151e1ef5f15ec940
Mustafa Dane <mustafadane@gmail.com>
2019-01-27 15:00:10 -0600

    version 1.0 is complete

--- client/Generator.js
+++ client/Generator.js
@@ -14,16 +14,40 @@ export class Generator extends Component {
     }
     async componentDidMount() {
         const { data:accessToken } = await axios.get('/token')
-        const { data: old } = await axios.get(`https://graph.facebook.com/v3.2/me/photos?fields=images&since=2008-01-01&until=2010-01-01&access_token=${accessToken}`)
+        const { data: old } = await axios.get(`https://graph.facebook.com/v3.2/me/photos?fields=images&since=2009-01-01&until=2010-01-01&access_token=${accessToken}`)
 
         const { data: now } = await axios.get(`https://graph.facebook.com/v3.2/me/photos?fields=images&since=2018-01-01&until=now&access_token=${accessToken}`)
 
         // console.log(old.data[Math.floor(Math.random() * old.data.length)])
         // console.log(now.data)
 
+        const filterCb = element => {
+            return element.height > 400 && element.height < 500
+        }
 
-        const oldUrl = old.data[Math.floor(Math.random() * old.data.length)].images[1].source
-        const newUrl = now.data[Math.floor(Math.random() * now.data.length)].images[1].source
+
+        const oldUrl = old.data[Math.floor(Math.random() * old.data.length)].images.filter(filterCb)[0].source
+        const newUrl = now.data[Math.floor(Math.random() * now.data.length)].images.filter(filterCb)[0].source
+
+        this.setState({oldUrl, newUrl, isLoading: false, accessToken})
+    }
+
+    handleClick = async () => {
+        this.setState({isLoading: true})
+        const { data: old } = await axios.get(`https://graph.facebook.com/v3.2/me/photos?fields=images&since=2009-01-01&until=2010-01-01&access_token=${this.state.accessToken}`)
+
+        const { data: now } = await axios.get(`https://graph.facebook.com/v3.2/me/photos?fields=images&since=2018-01-01&until=now&access_token=${this.state.accessToken}`)
+
+        // console.log(old.data[Math.floor(Math.random() * old.data.length)])
+        // console.log(now.data)
+
+        const filterCb = element => {
+            return element.height > 400 && element.height < 500
+        }
+
+
+        const oldUrl = old.data[Math.floor(Math.random() * old.data.length)].images.filter(filterCb)[0].source
+        const newUrl = now.data[Math.floor(Math.random() * now.data.length)].images.filter(filterCb)[0].source
 
         this.setState({oldUrl, newUrl, isLoading: false})
     }
@@ -40,10 +64,16 @@ export class Generator extends Component {
         return (
             <div>
                 <h1>Here is your #10YearChallenge :)</h1>
-                <img src={this.state.oldUrl} />
-                <img src={this.state.newUrl} />
-                {/* <h1>old:{this.state.oldUrl}</h1>
-                <h1>new:{this.state.newUrl}</h1> */}
+                <div className='final'>
+                    <div className='imageContainer'>
+                        <img src={this.state.oldUrl} />
+                    </div>
+                    <div className='imageContainer'>
+                        <img src={this.state.newUrl} />
+                    </div>
+                </div>
+                <br></br>
+                <button onClick={this.handleClick}>I don't like it!</button>
             </div>
         )
     }
